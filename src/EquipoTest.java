43cd98d9db3f54cf8dddfc1a2a5d82d799c3b166
angelruizmorales <angel.s265566@cesurformacion.com>
2022-01-24 19:45:13 +0100

    Create EquipoTest.java

---
+++ src/EquipoTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EquipoTest {
+
+	@Test
+	void testSetNombreEquipo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetRanking() {
+		fail("Not yet implemented");
+	}
+
+}
