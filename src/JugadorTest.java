3336000d781917185902edf24bad54d133daecd2
angelruizmorales <angel.s265566@cesurformacion.com>
2022-01-24 19:45:15 +0100

    Create JugadorTest.java

---
+++ src/JugadorTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JugadorTest {
+
+	@Test
+	void testSetNombreJugador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEdad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetIdioma() {
+		fail("Not yet implemented");
+	}
+
+}
