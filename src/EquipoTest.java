89efbc200577f55e387b1aed19ba9e4209a6336b
piyi-juanjis <94360229+piyi-juanjis@users.noreply.github.com>
2022-01-25 13:18:20 +0100

    equipo nombre
    cambios en el java

--- src/EquipoTest.java
+++ src/EquipoTest.java
@@ -4,15 +4,73 @@ import org.junit.jupiter.api.Test;
 
 class EquipoTest {
 
-	@Test
-	void testSetNombreEquipo() {
-		fail("Not yet implemented");
-	}
+	import static org.junit.jupiter.api.Assertions.*;
 
-	@Test
-	void testSetRanking() {
-		fail("Not yet implemented");
-	}
+	import org.junit.jupiter.api.Test;
 
-}
-//pruebaaaaaaaaaaaa
+	class EquipoTest {
+
+		@Test
+		void testSetnombreEquiponumerodecaracteressuperiora20() {
+			Equipo equipo = new Equipo();
+			String nombreEquipo = "Fcbarrio90123456789012345";
+			equipo.setnombreEquipo(nombreEquipo);
+			
+			assertEquals(null,equipo.getnombreEquipo());
+				
+		}
+		
+		@Test
+		void testSetnombreEquiponumerodecaracteresmenora20() {
+			Equipo equipo = new Equipo();
+			String nombreEquipo = "Fcbarrio";
+			equipo.setnombreEquipo(nombreEquipo);
+			
+			assertEquals(equipo.getnombreEquipo());
+				
+		}
+		
+		@Test
+		void testSetnombreEquiponumerodecaracteres20() {
+			Equipo equipo = new Equipo();
+			String nombreEquipo = "Fcbarrio901234567890";
+			equipo.setnombreEquipo(nombreEquipo);
+			
+			assertEquals(equipo.getnombreEquipo());
+				
+		}
+		
+		
+		@Test
+		void testSetnombreEquiponumerodecaracteresvacio() {
+			Equipo equipo = new Equipo();
+			String nombreEquipo = "";
+			equipo.setnombreEquipo(nombreEquipo);
+			
+			assertEquals(null,equipo.getnombreEquipo());
+				
+		}
+		
+		@Test
+		void testSetnombreEquipoyaregistrado() {
+			Equipo equipo = new Equipo();
+			String nombreEquipo = "Fcbarrio";
+			equipo.setnombreEquipo(nombreEquipo);
+			
+			assertEquals(null,equipo.getnombreEquipo());
+				
+		}
+		
+		
+
+		@Test
+		void testSetEdad() {
+			fail("Not yet implemented");
+		}
+
+		@Test
+		void testSetIdioma() {
+			fail("Not yet implemented");
+		}
+
+	}
