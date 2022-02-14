3343903835f5f9c43d7486cef44d7a0c48de47a7
leeeooo21 <94920768+leeeooo21@users.noreply.github.com>
2022-02-14 12:06:15 +0100

    story 3
    fran y leo estructura del código

--- src/Equipo.java
+++ src/Equipo.java
@@ -1,9 +1,9 @@
 
 public class Equipo {
+	
 	private String nombreEquipo;
 	private int ranking;
 
-	
 	
 	public Equipo () {
 	}
@@ -11,40 +11,49 @@ public class Equipo {
 	
 	public void setNombreEquipo(String nombreEquipo) {
 		
-		final int CARACTERES_NOMBRE_EQUIPO=20;
-		final int CARACTERES_NOMBRE_EQUIPO2=4;
+		final int CARACTERES_NOMBRE_EQUIPO1 = 20;
+		final int CARACTERES_NOMBRE_EQUIPO2 = 4;
+		final int CARACTERES_NOMBRE_EQUIPO3 = 3;
 		
 		
 		if(nombreEquipo.equals(" ")) {
 			this.nombreEquipo=null;
 		}
-		if(nombreEquipo.length()>=CARACTERES_NOMBRE_EQUIPO) {
-			this.nombreEquipo=null;
+		
+		if(nombreEquipo.length() >= CARACTERES_NOMBRE_EQUIPO1) {
+			this.nombreEquipo = null;
 		}
-		if(nombreEquipo.length()>=CARACTERES_NOMBRE_EQUIPO2) {
-			this.nombreEquipo=nombreEquipo;
+		
+		if(nombreEquipo.length() >= CARACTERES_NOMBRE_EQUIPO2) {
+			this.nombreEquipo = nombreEquipo;
 		}
-		if(nombreEquipo.length()>20) {
-			this.nombreEquipo=null;
+		
+		if(nombreEquipo.length() > 20) {
+			this.nombreEquipo = null;
 		}
+		
 		if(nombreEquipo.equals("Fcbarrio90")) {
-			this.nombreEquipo=null;
+			this.nombreEquipo = null;
 		}
 	}
 
 	
 	public void setRanking(int ranking) {
-	int rankingVacio = -1;
 		
+	int rankingVacio = -1;	
 	final int CARACTERES_NOMBRE_EQUIPO3=0;
 	
-		if(ranking<=10) {
+		if(ranking <= 10) {
 			this.ranking = ranking;
-		}else if(ranking==rankingVacio) {
+		}
+		
+		else if(ranking == rankingVacio) {
 			this.ranking = rankingVacio;
-		}else if(ranking< CARACTERES_NOMBRE_EQUIPO3) {
+		}
+		else if(ranking < CARACTERES_NOMBRE_EQUIPO3) {
 			this.ranking = rankingVacio;
-		}else {
+		}
+		else {
 			this.ranking = ranking;
 		}
 
@@ -58,20 +67,20 @@ public class Equipo {
 	}
 	public String categoriaEquipo(){
 		
-		final int CARACTERES_NOMBRE_EQUIPO4=-1;
-		final int CARACTERES_NOMBRE_EQUIPO5=3;
-		final int CARACTERES_NOMBRE_EQUIPO6=6;
-		final int CARACTERES_NOMBRE_EQUIPO7=10;
+		final int CARACTERES_NOMBRE_EQUIPO4 = -1;
+		final int CARACTERES_NOMBRE_EQUIPO5 = 3;
+		final int CARACTERES_NOMBRE_EQUIPO6 = 6;
+		final int CARACTERES_NOMBRE_EQUIPO7 = 10;
 		
-		if(nombreEquipo==null) {
+		if(nombreEquipo == null) {
 			return null;
-		}else if(ranking==CARACTERES_NOMBRE_EQUIPO4) {
+		}else if(ranking == CARACTERES_NOMBRE_EQUIPO4) {
 			return null;
-		}else if(ranking<CARACTERES_NOMBRE_EQUIPO5) {
+		}else if(ranking < CARACTERES_NOMBRE_EQUIPO5) {
 			return "Tercera";
-		}else if(ranking>=3&&ranking<=CARACTERES_NOMBRE_EQUIPO6) {
+		}else if(ranking >=3 && ranking <= CARACTERES_NOMBRE_EQUIPO6) {
 			return "Segunda";
-		}else if(ranking>=7&&ranking<=CARACTERES_NOMBRE_EQUIPO7) {
+		}else if(ranking >=7 && ranking <= CARACTERES_NOMBRE_EQUIPO7) {
 			return "Primera";
 		}else {
 			return null;
