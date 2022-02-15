3b2e4b5db7510c5d6d29865edbadfbf8efda0b70
angelruizmorales <angel.s265566@cesurformacion.com>
2022-02-15 18:03:29 +0100

    jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -4,70 +4,66 @@ public class Jugador {
 	private int edad;
 	private String idioma;
 	private String tipojugador;
-
+	
 
 
 	public Jugador () {
 
 	}
-	
-	
+
+
 
 	public void setNombreJugador(String nombre) {
 		//tener en cuenta que tiene que ir en mayusculas
 
 		boolean tieneNumeros = nombre.matches("[0-9].+");
-		
+
 		if (!tieneNumeros && nombre.length() <=20 && nombre.length() >= 4){
 			this.nombre = nombre.toUpperCase();
-		}
-		else if (nombre == "@" || nombre.equals("")) {
+		} else if (nombre == "@" || nombre.equals("")) {
 			this.nombre = null;
-		}
-		else {
+		} else {
 			this.nombre = null;
 		}
 
 
-		
+
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 	public void setEdad(int edad) {
-		final int VACIO = -1;	
-
-		if( edad >= 18) {				//Jugador mayor de edad.
+		final int VACIO = -1;
+ 
+		if ( edad >= 18) {				//Jugador mayor de edad.
 			this.edad = edad;
-	
-		}else if( edad == -1) {		//Edad vacía.
+
+		} else if ( edad == -1) {		//Edad vacía.
 			this.edad = VACIO;
-		}
-		else {
+		} else {
 			this.edad = VACIO;
 		}
 	}
-	
-	
+
+
 	public void setIdioma(String idioma) {	
-		//Idiomas correcto e incorrecto.
-		final String IDIOMA_VALIDO[]=	{ "Inglés" "Español"}
-		if(idioma.equals("Español")) {
-			this.idioma = idioma;
-		}else if(idioma.equals("Inglés")) {
-			this.idioma = idioma;
-		}else if(idioma.equals("Aleman")) {
-			this.idioma = idioma;
-		}else if(idioma.equals("Francés")) {
-			this.idioma = idioma;
-		}else if(idioma.equals("")) {     				//Idioma vacío.
-			this.idioma = null;
-		}else if(idioma.equals("Español Inglés")) {    //Elegir dos idiomas.
-			this.idioma = null;
+		final String IDIOMA_VALIDO[]=	
+			{ 
+					"Inglés","Español","Aleman","Francés"		
+			};
+		if (idioma.equals( IDIOMA_VALIDO[0] ) 
+				|| idioma.equals( IDIOMA_VALIDO[1] ) 
+				|| idioma.equals( IDIOMA_VALIDO[2] )
+				|| idioma.equals( IDIOMA_VALIDO[3] )) {
+			this.idioma=idioma;
+		} else {
+			this.idioma=null;
 		}
+
 	}
+
 
 
 
@@ -81,21 +77,17 @@ public class Jugador {
 		return edad;
 	}
 	public String tipoJugador() {
-		if(nombre==null) {
+		final int VACIO = -1;
+
+		if ( nombre == null || idioma == null || edad == VACIO || edad < 18 ) {
 			return null;
-		}else if(idioma==null) {
-			return null;
-		}else if(edad==-1) {
-			return null;
-		}else if(edad<18) {
-			return null;
-		}else if(edad>=18&&edad<25){
+		} else if ( edad >= 18 && edad < 25 ){
 			return "Junior";
-		}else if(edad>=25&&edad<35){
+		} else if ( edad >= 25 && edad < 35 ){
 			return "Senior";
-		}else if(edad>=35){
+		} else if ( edad >= 35 ){
 			return "Master";
-		}else {
+		} else {
 			return null;
 		}
 
